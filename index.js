44d4e3f220118d7d35cb2d7260b90f56739e0b04
Durjoy-dcp <unknownccxv@gmail.com>
2022-11-25 21:10:58 +0600

    wishlist putting

--- index.js
+++ index.js
@@ -19,26 +19,98 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const userCollection = client.db('carhub').collection('user');
+        const productCollection = client.db('carhub').collection('product');
+        const wishListCollection = client.db('carhub').collection('wishlist');
+        const myorderCollection = client.db('carhub').collection('myorder');
         const verifySeller = async (req, res, next) => {
-            console.log(req.query.email)
+            // console.log(req.query.email)
             const email = req.query.email;
             const query = { email: email };
             const user = await userCollection.findOne(query);
             if (user?.role !== 'Seller') {
 
                 req.role = ''
+                req.verified = false
             } else {
+                req.verified = false
                 req.role = 'Seller'
+                console.log(user.verifiedSeller)
+                if (user.verifiedSeller) {
+                    req.verified = true
+                }
+            }
+            next();
+        }
+        const verifyAdmin = async (req, res, next) => {
+            console.log(req.query.email)
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            if (user?.role !== 'admin') {
+
+                req.role = ''
+            } else {
+                req.role = 'admin'
             }
             next();
         }
 
         app.get('/user/seller', verifySeller, async (req, res) => {
-            console.log(req?.role)
-            res.send({ isSeller: req?.role === 'Seller' });
+            console.log(req?.role, req.verified)
+            res.send({ isSeller: req?.role === 'Seller', verified: req.verified });
 
         })
+        app.get('/product', async (req, res) => {
+            const email = req.query.email;
+            const query = {
+                email: email
+            }
+            const products = await productCollection.find(query).toArray();
+            res.send(products);
+        })
 
+        app.get('/user/admin', verifyAdmin, async (req, res) => {
+            console.log(req?.role)
+            res.send({ isAdmin: req?.role === 'admin' });
+
+        })
+        app.post('/productadd', async (req, res) => {
+            const data = req.body;
+            data.date = new Date(Date.now()).toISOString();
+            const resut = await productCollection.insertOne(data);
+            res.send(resut);
+        })
+        app.put('/wishlist', async (req, res) => {
+            // const data = req.body;
+            // const resut = await wishListCollection.insertOne(data);
+            // res.send(resut);
+            // , serial: user._id
+            const user = req.body;
+            const filter = { email: user.email, serial: user._id };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    serial: user._id,
+                    email: user.email,
+                    catagory: user.catagory,
+                    img: user.img,
+                    name: user.name,
+                    price: user.price
+
+
+                }
+            };
+            const result = await wishListCollection.updateOne(filter, updateDoc, options);
+            console.log(result);
+            res.send(result)
+        })
+        app.get('/catagory/:id', async (req, res) => {
+            const catagory = req.params.id;
+            const query = { catagory: catagory };
+            const result = await productCollection.find(query).toArray();
+            console.log(result);
+            res.send(result);
+        })
         app.put('/user', async (req, res) => {
             const user = req.body;
             const filter = { email: user.email };
@@ -48,7 +120,8 @@ async function run() {
                     email: user.email,
                     role: user.role,
                     name: user.name,
-                    img: user.img
+                    img: user.img,
+                    verifiedSeller: false
                 },
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
